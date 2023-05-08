a673fa8bebf39234ea15c78ac6e025a8afe1a006
Ziyodullo Aliyev <89968839+Ziyodullodev@users.noreply.github.com>
2023-05-08 11:31:38 +0500

    Remove ignored files from repository

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import localsettings as ls
+import local_settings as ls
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
--- localsettings.py
+++
@@ -1,4 +0,0 @@
-SECRET_KEY = "3gr24uq+2ri8ox0ub+#f9gezk#&)&k0s#n&!da=f1tc6&6g+9x"
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
